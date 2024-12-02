b389d60ff93fb79001a0d1704ff460e22d7a78b4
Sampson David <davidchosen33@gmail.com>
2024-12-02 16:05:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,207 @@
+
+
+const hh =  [ "ukyfyghfgv", "ukyfyghfgv", "hilgvuhfv"  ]
+
+const uu = [ 12, 56, 78, 90 ]
+
+const  oo = [ "hfyfv", "hgvhgb", true, 123 ]
+
+// console.log(hh[0], hh[1], hh[2]) 
+
+hh.push("David")
+
+// console.log(hh)
+
+hh.pop()
+
+// console.log(hh)
+
+hh.unshift("yyyyy")
+
+// console.log(hh)
+
+hh.includes("")
+
+// Objects
+
+// const yy = {
+//     name: "",
+//     age: 33,
+//     school: "Uniport",
+//     isTall: true,
+//     graduated: null,
+//     skilss: [ 
+//         "hfyfv", 
+//         "hgvhgb", 
+//         true, 
+//         123,
+//         {
+//             gg: "66",
+//             hh: false,
+//         } 
+//     ]
+// }
+
+// console.log(yy.skilss[4].hh)
+
+// const ss1 = ["david", "sampson", "hhh"]
+
+// While
+// For
+
+// when to start,
+// When to stop
+// Incrementing the variable
+
+// let x = 0
+
+
+// while(x < 3 ){
+
+//     console.log("first")
+
+//     x++
+
+// }
+
+// const ss2 = ["david", "sampson", "hhh"]
+
+// for(let a = 0; a < 3; a++){
+//     console.log(ss2[a])
+// }
+
+
+// Condtionals
+
+// If Else statements
+
+// const d = 15
+
+// if(d < 6){
+//     console.log("Good Morning")
+// } else if(d == 10) {
+//     console.log("Good afternoon")
+// } else {
+//     console.log("Good Evening")
+// }
+
+// const x = 3
+
+// const y = 1
+
+// const z = 20  
+
+// if(y > 0 || z < 4 && x >= 5 ){
+//     console.log("first")
+// } else {
+//     console.log("Second")
+// }if
+
+// const gg = "1"
+
+
+// if(gg == true){
+//     console.log("first")
+// } else {
+//     console.log("Second")
+// }
+
+
+// const y = 2
+// const x = false 
+
+// x ? console.log("Yes") : console.log("No")
+
+// y < 2 ? console.log("true") : console.log("False")
+
+// const x = 4
+
+
+// x <= 4 ? console.log(x + 1) : console.log(x - 1)
+
+
+//   const message = "successful"
+
+//   message === "successful" ? console.log("green") : console.log("red")
+
+
+
+// const a = "green"
+
+// switch(a){
+//     case "red":
+//         console.log("red") 
+//         break
+
+//     case "blue": 
+//         console.log("blue")
+//     break
+
+//     default: 
+//         console.log("nothing")
+
+
+// }
+
+
+
+function addNumbers (){
+    let answer 
+    const a = 4
+    const b = 6
+    const c = 7
+
+    answer = a**b * c **3
+
+    console.log(answer)
+}
+
+// addNumbers()
+
+// greet("Grerta", 77)
+
+
+// function greet (name, age){
+//     const msg = `Hello ${name}, your age is ${age}`
+//     console.log(msg)
+// }
+
+// doSomething(1, 4)
+
+// function doSomething (num, num2){
+//     const ans = num + num2 
+//     console.log(ans)
+// }
+
+
+const jhkftyffv = (num = 4, num2 = 2)=>{
+    const ans = num + num2
+    console.log(ans)
+
+}
+
+jhkftyffv(7,)
+jhkftyffv()
+jhkftyffv(3,7)
+
+const check = ()=>{
+    const string = "hvuhf"
+    const amount = 40
+
+    if(string.length < 6){
+        console.log("String must be 6 charas Min")
+    }
+
+    
+}
+
+
+check()
+
+
+
+
+
+
+
+
